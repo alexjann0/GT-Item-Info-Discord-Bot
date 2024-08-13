35b8a34b8f02711735d0fd757b673b1ebf7fed55
alex <168668310+alexjann0@users.noreply.github.com>
2024-08-13 21:14:23 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { Client, IntentsBitField } = require('discord.js');
+const ready = require('./Events/Ready/ready');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+ready(client);
+
+client.login(process.env.BOT_TOKEN);
